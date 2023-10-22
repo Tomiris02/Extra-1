a8ef9ac84edbda141d3f97bd397456d984dd7fef
Томирис <tomiris@MBP-Tomiris.fritz.box>
2023-10-22 20:03:50 +0200

    Lab-1

---
+++ Desktop/lab_1.py
@@ -0,0 +1,18 @@
+# Шаг 1: Приветственное сообщение
+print("Hello, welcome to our school")
+
+# Шаг 2: Заполнение данных
+name = input("Your name: ")
+surname = input("Your surname: ")
+birthday = input("Date of your birth: ")
+question_1 = input("How did you find us: ")
+question_2 = input("Do you have something to add: ")
+
+# Шаг 3: Отображение всех заполненных данных
+print("\nThanks! You've successfully entered your data. Your data:")
+print("Name " + name)
+print("Surname: " + surname)
+print("Birthday: " + birthday)
+print("Answer for q1: " + question_1)
+print("Answer for q2: " + question_2)
+
